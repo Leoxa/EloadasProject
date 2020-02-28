c17dd8806760ed8b98a48f700774ec8deb39f9e3
Galgóczi Martin <martingalgoczi2@gmail.com>
2020-02-28 16:53:11 +0100

    Kész

---
+++ ClassLibrary1/Eloadas.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace EloadasProject
+{
+    public class Eloadas
+    {
+        private bool[,] foglalasok;
+        private int sorokSzama;
+        private int helyekSzama;
+
+        public Eloadas(int sorokSzama, int helyekSzama)
+        {
+            if (sorokSzama > 0 && helyekSzama > 0)
+            {
+                this.sorokSzama = sorokSzama;
+                this.helyekSzama = helyekSzama;
+                foglalasok = new bool[sorokSzama, helyekSzama];
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        foglalasok[i, j] = false;
+                    }
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Nem lehet 0");
+            }
+        }
+
+        public bool Foglal()
+        {
+            for (int i = 0; i < sorokSzama; i++)
+            {
+                for (int j = 0; j < helyekSzama; j++)
+                {
+                    if (foglalasok[i, j] == false)
+                    {
+                        foglalasok[i, j] = true;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public int SzabadHelyek
+        {
+            get
+            {
+                int osszeg = 0;
+                for (int i = 0; i < sorokSzama; i++)
+                {
+                    for (int j = 0; j < helyekSzama; j++)
+                    {
+                        if (foglalasok[i, j] == false)
+                        {
+                            osszeg++;
+                        }
+                    }
+                }
+                return osszeg;
+            }
+        }
+
+        public bool Full()
+        {
+            return SzabadHelyek == 0;
+        }
+
+        public bool Foglalt(int sorSzam, int helySzam)
+        {
+            if (sorSzam > 0 && helySzam > 0 && sorSzam <= this.sorokSzama && helySzam <= this.helyekSzama)
+            {
+                return this.foglalasok[sorSzam--, helySzam--];
+            }
+            else
+            {
+                throw new ArgumentException("Nem lehet 0");
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Test/EloadasTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace EloadasProject.Tests
+{
+    [TestFixture]
+    class EloadasTest
+    {
+        Eloadas eloadas;
+
+        [SetUp]
+        public void SetUp()
+        {
+            eloadas = new Eloadas(10, 10);
+        }
+
+
+        [TestCase]
+        public void Helyekszamacsokkenfoglalakor()
+        {
+            eloadas.Foglal();
+            Assert.AreEqual(99, eloadas.SzabadHelyek, "Nem csökkentek a szabad helyek");
+        }
+
+        [TestCase]
+        public void UjEloadas()
+        {
+            Assert.AreEqual(100, eloadas.SzabadHelyek, "Hibás a szabad helyek száma");
+        }
+        [TestCase]
+        public void UresEloadas()
+        {
+            Assert.IsFalse(eloadas.Full(), "Nem üres az előadás");
+        }
+
+        [TestCase]
+        public void TeliEloadas()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    eloadas.Foglal();
+                }
+            }
+            Assert.IsTrue(eloadas.Full(), "Tele van az előadás");
+        }
+
+        [TestCase]
+        public void NemLehetNegativ()
+        {
+            Assert.Throws<ArgumentException>(() =>{var eloadas = new Eloadas(-2, -2);});
+        }
+
+        [TestCase]
+        public void NemLehetKevesebb()
+        {
+            Assert.Throws<ArgumentException>(() => {eloadas.Foglalt(0, 0);});
+        }
+
+        [TestCase]
+        public void NemLehetTobbAMegadottMennyisegnel()
+        {
+            Assert.Throws<ArgumentException>(() =>{eloadas.Foglalt(11, 11);});
+        }
+    }
+}
